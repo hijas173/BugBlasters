cf62e3d74a62faea7e8a0ad657ed622cd199f418
FelixYizhu <49159678+FelixYizhu@users.noreply.github.com>
2019-08-07 22:45:49 +1000

    Update BorrowBookControl.java
    Modify borrowCancel() method code block and modify the indentation.

--- ITC205_201960_Asg2_Baseline_Code/BorrowBookControl.java
+++ ITC205_201960_Asg2_Baseline_Code/BorrowBookControl.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BorrowBookControl {
-	
 	private BorrowBookUI borrowBookUi; //'UI' changed to 'borrowBookUi'
-	
 	private Library library; //'library' changed to 'Library','LIBRARY' changed to 'library' 
 	private Member member; //'member' changed to 'Member','M' changed to 'member'
 	private enum ControlState {INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED};//'CONTROL_STATE' changed to 'ControlState', delete the white spance	
@@ -13,14 +11,10 @@ public class BorrowBookControl {
 	private List<Book> pendingBooks; //'PENDING' changed to 'pendingBooks','book' changed to 'Book'
 	private List<Loan> completedBooks;//'COMPLETED' changed to 'completedBooks','loan' changed to 'Loan'
 	private Book book; //'book' changed to 'Book','BOOK' changed to 'book'
-	
-	
 	public BorrowBookControl() {
 		this.library = Library.getInstance();//'LIBRARY' changed to 'library','LIBRARY' changed to 'Library','INSTANCE()' changed to 'getInstance()'
 		controlState = ControlState.INITIALISED;//'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
 	}
-	
-
 	public void setUI(BorrowBookUI ui) {
 		if (!controlState.equals(ControlState.INITIALISED)) //'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
 			throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
@@ -29,12 +23,9 @@ public class BorrowBookControl {
 		borrowBookUi.Set_State(BorrowBookUI.UiState.READY);//'UI' changed to 'borrowBookUi','UI_STATE' changed to 'UiState'
 		controlState = ControlState.READY;	//'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'	
 	}
-
-		
 	public void cardSwiped(int memberId) {//'Swiped' changed to 'cardSwiped','MEMMER_ID' changed to 'memberId'
 		if (!controlState.equals(ControlState.READY)) //'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
 			throw new RuntimeException("BorrowBookControl: cannot call cardSwiped except in READY state");
-			
 		member = Library.getMember(memberId);//'M' changed to 'member','LIBRARY' changed to 'Library','MEMBER' changed to 'getMember','MEMMER_ID' changed to 'memberId'
 		if (member == null) {//'M' changed to 'member'
 			borrowBookUi.Display("Invalid memberId");//'UI' changed to 'borrowBookUi'
@@ -50,8 +41,6 @@ public class BorrowBookControl {
 			borrowBookUi.setState(BorrowBookUI.UiState.RESTRICTED); //'UI' changed to 'borrowBookUi','Set_State' changed to 'setState','UI_STATE' changed to 'UiState'
 		}
 	}//bracketing style keep consistent
-	
-	
 	public void bookScanned(int bookId) {//'Scanned' changed to 'bookScanned'
 		book = null;//'BOOK' changed to 'book'
 		if (!controlState.equals(ControlState.SCANNING)) {//'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
@@ -75,8 +64,6 @@ public class BorrowBookControl {
 			borrowComplete();//'Complete' changed to 'borrowComplete'
 		}
 	}
-	
-	
 	public void borrowComplete() {//'Complete' changed to 'borrowComplete'
 		if (pendingBooks.size() == 0) {//'PENDING' changed to 'pendingBooks'
 			borrowCancel();//'cancel' changed to 'borrowCancel'
@@ -91,8 +78,6 @@ public class BorrowBookControl {
 			controlState = ControlState.FINALISING;//'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
 		}
 	}
-
-
 	public void commitLoans() {//'Commit_LOans' changed to 'commitLoans'
 		if (!controlState.equals(ControlState.FINALISING)) {//'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
 			throw new RuntimeException("BorrowBookControl: cannot call commitLoans except in FINALISING state");
@@ -109,11 +94,9 @@ public class BorrowBookControl {
 		controlState = ControlState.COMPLETED;//'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
 	}
 
-	
 	public void borrowCancel() {//'cancel' changed to 'borrowCancel'
-		UI.Set_State(BorrowBookUI.UI_STATE.CANCELLED);
-		controlState = CONTROL_STATE.CANCELLED;//'State' changed to 'controlState'
+		borrowBookUi.setState(BorrowBookUI.UiState.CANCELLED);//'UI' changed to 'borrowBookUi','Set_State' changed to 'setState','UI_STATE' changed to 'UiState'
+		controlState = ControlState.CANCELLED;//'State' changed to 'controlState','CONTROL_STATE' changed to 'ControlState'
 	}
-	
 	
 }
